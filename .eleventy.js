134ba32ca6c238615ca167799736e13363868d5a
itsnicola <68837907+itsnicola@users.noreply.github.com>
2025-10-13 14:43:48 +0200

    fix fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("src/assets/photos");
-  // eleventyConfig.addPassthroughCopy("src/assets/fonts"); Try font-face CSS again w this
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  
   return {
     dir: {
       input: "src",
